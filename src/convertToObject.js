99e7fd71ff5b3cc2efa380a6c0502b7ada4bed76
Oksana Shtelma <oshtelma@gmail.com>
2026-02-27 20:12:47 +0200

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,44 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  let x;
+
+  const filtered = sourceString
+    .split(';')
+    .map((el, i, ar) => {
+      return ar[i].trim();
+    })
+    .filter((el) => {
+      return el !== ';' && el !== '';
+    });
+
+  const resultObj = filtered
+    .map((el, i, ar) => {
+      return el.split(':');
+    })
+    .map((elem) => {
+      elem.map((e, i, a) => {
+        elem[1] = a[1].trim();
+        elem[0] = a[0].trim();
+
+        return e;
+      });
+
+      return { [elem[0]]: elem[1] };
+    });
+
+  resultObj.map((el, i, ar) => {
+    for (const key in ar[i]) {
+      x = resultObj[i][key].trim();
+      resultObj[i][key] = x;
+    }
+  });
+
+  const mergedObject = resultObj.reduce((acc, curr) => {
+    return { ...acc, ...curr };
+  }, {});
+
+  return mergedObject;
 }
 
 module.exports = convertToObject;
