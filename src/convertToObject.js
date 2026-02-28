7d9054d4a7cd1394071e758c507eb76bdac5d649
Oksana Shtelma <oshtelma@gmail.com>
2026-02-28 15:22:06 +0200

    solution changed

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,8 +6,6 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  let x;
-
   const filtered = sourceString
     .split(';')
     .map((el, i, ar) => {
@@ -17,29 +15,23 @@ function convertToObject(sourceString) {
       return el !== ';' && el !== '';
     });
 
-  const resultObj = filtered
-    .map((el, i, ar) => {
-      return el.split(':');
-    })
-    .map((elem) => {
-      elem.map((e, i, a) => {
-        elem[1] = a[1].trim();
-        elem[0] = a[0].trim();
-
-        return e;
-      });
-
-      return { [elem[0]]: elem[1] };
-    });
-
-  resultObj.map((el, i, ar) => {
-    for (const key in ar[i]) {
-      x = resultObj[i][key].trim();
-      resultObj[i][key] = x;
-    }
+  const keyValuePairs = filtered.map((el, i, ar) => {
+    return el.split(':');
   });
 
-  const mergedObject = resultObj.reduce((acc, curr) => {
+  const keyValueObjects = keyValuePairs.map(([key, value]) => {
+    return { [key.trim()]: value.trim() };
+  });
+
+  const cleaned = keyValueObjects.map((obj) => {
+    return Object.fromEntries(
+      Object.entries(obj).map(([k, v]) => {
+        return [k, v.trim()];
+      }),
+    );
+  });
+
+  const mergedObject = cleaned.reduce((acc, curr) => {
     return { ...acc, ...curr };
   }, {});
 
